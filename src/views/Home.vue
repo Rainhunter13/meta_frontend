4ed1fb1d92231132d36300a34c2f0123a44755a7
Rakhman Asmatullayev <rakhmandva@gmail.com>
2021-02-11 00:06:50 +0600

    Profile links added to the home page instead of methods and photo_url, Home: green -> black

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,17 @@
     <h1>
       All available therapists are as follow:
     </h1>
-    <a class="therapist"> ID - Name - Methods - Photo URL</a> <br />
+    <a class="therapist"> ID - Name - Profile Link</a> <br />
     <hr style="width:50%;text-align:left;margin-left:0" />
     <span v-for="therapist in all_therapists" :key="therapist">
       <a class="therapist"> {{ therapist["id"] }} - </a>
       <a class="therapist"> {{ therapist["name"] }} - </a>
-      <a class="therapist"> {{ therapist["methods"] }} - </a>
-      <a class="therapist"> {{ therapist["photo_url"] }} </a>
+      <a
+        class="therapist"
+        :href="'http://localhost:8080/#/therapist/' + therapist['id']"
+      >
+        http://localhost:8080/#/therapist/{{ therapist["id"] }}
+      </a>
       <br />
     </span>
   </div>
